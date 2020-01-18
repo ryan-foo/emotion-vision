fbdbd0d27c6a373362d0248293a47b7359668157
VishnuSarath <44475512+VishnuSarath@users.noreply.github.com>
2020-01-18 16:18:19 +0800

    android app camera

---
+++ hack2020/app/src/main/java/com/example/hack2020/MainActivity.java
@@ -0,0 +1,260 @@
+package com.example.hack2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+    File photoFile = null;
+    static final int CAPTURE_IMAGE_REQUEST = 1;
+
+    String mCurrentPhotoPath;
+    private static final String IMAGE_DIRECTORY_NAME = "VLEMONN";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            captureImage();
+        }
+        else
+        {
+            captureImage2();
+        }
+    }
+
+
+
+    /* Capture Image function for 4.4.4 and lower. Not tested for Android Version 3 and 2 */
+    private void captureImage2() {
+
+        try {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            photoFile = createImageFile4();
+            if(photoFile!=null)
+            {
+                displayMessage(getBaseContext(),photoFile.getAbsolutePath());
+                Log.i("Mayank",photoFile.getAbsolutePath());
+                Uri photoURI  = Uri.fromFile(photoFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, CAPTURE_IMAGE_REQUEST);
+            }
+        }
+        catch (Exception e)
+        {
+            displayMessage(getBaseContext(),"Camera is not available."+e.toString());
+        }
+    }
+
+    private void captureImage()
+    {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+        else
+        {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                try {
+
+                    photoFile = createImageFile();
+                    displayMessage(getBaseContext(),photoFile.getAbsolutePath());
+                    Log.i("Mayank",photoFile.getAbsolutePath());
+
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        // Toast.makeText(getApplicationContext(), "photofile!=null", Toast.LENGTH_LONG).show();
+                        Uri photoURI = FileProvider.getUriForFile(this,
+                                "com.vlemonn.blog.captureimage",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST);
+                    }
+                } catch (Exception ex) {
+                    // Error occurred while creating the File
+                    displayMessage(getBaseContext(),ex.getMessage().toString());
+                }
+
+
+            }else
+            {
+                displayMessage(getBaseContext(),"Nullll");
+            }
+        }
+
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //Bundle extras = data.getExtras();
+        //Bitmap imageBitmap = (Bitmap) extras.get("data");
+        //imageView.setImageBitmap(imageBitmap);
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            new MyDownloader().execute();
+        } else {
+            displayMessage(getBaseContext(), "Request cancelled or something went wrong.");
+        }
+    }
+
+    private File createImageFile4()
+    {
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                displayMessage(getBaseContext(),"Unable to create directory.");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void displayMessage(Context context, String message)
+    {
+        //Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                captureImage();
+            }
+        }
+
+    }
+
+    public class MyDownloader extends AsyncTask<String,Void, Bitmap>
+
+    {
+
+        @Override
+
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+        }
+
+        @Override
+
+        protected void onPostExecute(Bitmap bitmap) {
+
+            super.onPostExecute(bitmap);
+
+        }
+
+        @Override
+
+        protected Bitmap doInBackground(String... params) {
+            final TextView ans = (TextView) findViewById(R.id.answer);
+            ans.setText("Image Captured");
+
+            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            // imageView.setImageBitmap(myBitmap);
+            String root = Environment.getExternalStorageDirectory().toString();
+            File myDir = new File(root);
+            myDir.mkdirs();
+            String fname = "Hack & Roll"+ ".jpg";
+            File file = new File(myDir, fname);
+
+
+
+
+            try {
+                InputStream imagesStream;
+                imagesStream = new FileInputStream(photoFile);
+
+
+
+            } catch (FileNotFoundException e) {
+                Log.i("ERROR", e.toString());
+            }
+            //ans.setText(file.toString());
+            if (file.exists()) file.delete();
+            // Log.i("LOAD", file.toString());
+
+            try {
+                FileOutputStream out = new FileOutputStream(file);
+                myBitmap.compress(Bitmap.CompressFormat.JPEG, 70, out);
+                out.flush();
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+}
